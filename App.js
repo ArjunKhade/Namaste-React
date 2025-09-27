7943facba7b9c9f336385a8316fa9f4bd2b5de6a
Arjun <khadearjun@gmail.com>
2025-09-27 19:37:33 +0530

    added initial code of react

---
+++ App.js
@@ -0,0 +1,9 @@
+
+// react java script object 
+const heading = React.createElement("h1",{ id:"heading", xyz:"abc"}, "Hello from Travg");
+
+//root of react
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// convert the object to the h1 tag and put in root element
+root.render(heading);
